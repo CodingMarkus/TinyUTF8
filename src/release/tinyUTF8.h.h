3a5b48608ad2daafdd6e018683cdea317d95bd00
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2023-11-13 10:31:12 +0100

    Add license statement to release files

--- src/release/tinyUTF8.h.h
+++ src/release/tinyUTF8.h.h
@@ -1,1 +1,12 @@
+/**
+
+	TinyUTF8 is licensed under the Apache License Version 2.0.
+
+	You should have received the source code together with a copy of this
+	license in a file called LICENSE.
+
+	If not, you can find this license online at the following URL:
+	http://www.apache.org/licenses/
+*/
+
 #include "coding.h"
