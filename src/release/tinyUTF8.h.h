9e1f606c1f0fcf238ad2f218a7db3180a576e849
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2023-12-30 20:36:25 +0100

    Update copyright statement

--- src/release/tinyUTF8.h.h
+++ src/release/tinyUTF8.h.h
@@ -1,12 +1,17 @@
 /**
+	Copyright 2023 CodingMarkus (TinyUTF8.2023@maha.mozmail.com)
 
-	TinyUTF8 is licensed under the Apache License Version 2.0.
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-	You should have received the source code together with a copy of this
-	license in a file called LICENSE.
+		http://www.apache.org/licenses/LICENSE-2.0
 
-	If not, you can find this license online at the following URL:
-	http://www.apache.org/licenses/
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
 */
 
 #include "coding.h"
