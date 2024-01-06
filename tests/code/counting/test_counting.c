32dadd749f57ef2840270f023eff4dd416b32b6c
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2024-01-06 01:08:17 +0100

    Fix test of counting

--- tests/code/counting/test_counting.c
+++ tests/code/counting/test_counting.c
@@ -2,93 +2,146 @@
 
 #include "counting.h"
 
-#include "test_strings.h"
+#include "test_stringsNF.h"
 
 #include "code.h"
 Begin
 // ============================================================================
 
 static
-void test_countBytes( )
+void test_count( )
 {
-	size_t countBytes = 0;
-	Error_TinyUTF8 error = No_Error_TinyUTF8;
+	for (size_t i = 0; testStringsNFC[i].string; i++) {
+		size_t count = 0;
+		Error_TinyUTF8 error = No_Error_TinyUTF8;
 
-	bool success = getStringCounts_TinyUTF8(
-		composedUTF8TestString,
-		&countBytes, NULL, NULL,
-		&error
-	);
+		bool success = getStringCounts_TinyUTF8(
+			testStringsNFC[i].string,
+			&count, NULL, NULL,
+			&error
+		);
 
-	testAssertMsg(success && !error, "Error: %s", nameOfError_TinyUTF8(error));
-	testAssertMsg(countBytes == 508, "Count: %zu", countBytes);
+		testAssertMsg(success && !error,
+			"Error: %s", nameOfError_TinyUTF8(error)
+		);
+		testAssertMsg(count == testStringsNFC[i].byteCount,
+			"Count: %zu, Expected: %zu",
+			count, testStringsNFC[i].byteCount
+		);
+	}
 
-	success = getStringCounts_TinyUTF8(
-		decomposedUTF8TestString,
-		&countBytes, NULL, NULL,
-		&error
-	);
+	for (size_t i = 0; testStringsNFD[i].string; i++) {
+		size_t count = 0;
+		Error_TinyUTF8 error = No_Error_TinyUTF8;
 
-	testAssertMsg(success && !error, "Error: %s", nameOfError_TinyUTF8(error));
-	testAssertMsg(countBytes == 525, "Count: %zu", countBytes);
+		bool success = getStringCounts_TinyUTF8(
+			testStringsNFD[i].string,
+			&count, NULL, NULL,
+			&error
+		);
+
+		testAssertMsg(success && !error,
+			"Error: %s", nameOfError_TinyUTF8(error)
+		);
+		testAssertMsg(count == testStringsNFD[i].byteCount,
+			"Count: %zu, Expected: %zu",
+			count, testStringsNFD[i].byteCount
+		);
+	}
 }
 
 
 static
 void test_countCodePoints( )
 {
-	size_t countCP = 0;
-	Error_TinyUTF8 error = No_Error_TinyUTF8;
+	for (size_t i = 0; testStringsNFC[i].string; i++) {
+		size_t count = 0;
+		Error_TinyUTF8 error = No_Error_TinyUTF8;
 
-	bool success = getStringCounts_TinyUTF8(
-		composedUTF8TestString,
-		NULL, &countCP, NULL,
-		&error
-	);
+		bool success = getStringCounts_TinyUTF8(
+			testStringsNFC[i].string,
+			NULL, &count, NULL,
+			&error
+		);
 
-	testAssertMsg(success && !error, "Error: %s", nameOfError_TinyUTF8(error));
-	testAssertMsg(countCP == 212, "Count: %zu", countCP);
+		testAssertMsg(success && !error,
+			"Error: %s", nameOfError_TinyUTF8(error)
+		);
+		testAssertMsg(count == testStringsNFC[i].codepointCount,
+			"Count: %zu, Expected: %zu",
+			count, testStringsNFC[i].codepointCount
+		);
+	}
 
-	success = getStringCounts_TinyUTF8(
-		decomposedUTF8TestString,
-		NULL, &countCP, NULL,
-		&error
-	);
+	for (size_t i = 0; testStringsNFD[i].string; i++) {
+		size_t count = 0;
+		Error_TinyUTF8 error = No_Error_TinyUTF8;
 
-	testAssertMsg(success && !error, "Error: %s", nameOfError_TinyUTF8(error));
-	testAssertMsg(countCP == 242, "Count: %zu", countCP);
+		bool success = getStringCounts_TinyUTF8(
+			testStringsNFD[i].string,
+			NULL, &count, NULL,
+			&error
+		);
+
+		testAssertMsg(success && !error,
+			"Error: %s", nameOfError_TinyUTF8(error)
+		);
+		testAssertMsg(count == testStringsNFD[i].codepointCount,
+			"Count: %zu, Expected: %zu",
+			count, testStringsNFD[i].codepointCount
+		);
+	}
+
 }
 
 
 static
 void test_countCharacters( )
 {
-	size_t countChars = 0;
-	Error_TinyUTF8 error = No_Error_TinyUTF8;
+	for (size_t i = 0; testStringsNFC[i].string; i++) {
+		size_t count = 0;
+		Error_TinyUTF8 error = No_Error_TinyUTF8;
 
-	bool success = getStringCounts_TinyUTF8(
-		composedUTF8TestString,
-		NULL, NULL, &countChars,
-		&error
-	);
+		bool success = getStringCounts_TinyUTF8(
+			testStringsNFC[i].string,
+			NULL, NULL, &count,
+			&error
+		);
 
-	testAssertMsg(success && !error, "Error: %s", nameOfError_TinyUTF8(error));
-	testAssertMsg(countChars == 212, "Count: %zu", countChars);
+		testAssertMsg(success && !error,
+			"Error: %s", nameOfError_TinyUTF8(error)
+		);
+		testAssertMsg(count == testStringsNFC[i].charaterCount,
+			"Count: %zu, Expected: %zu",
+			count, testStringsNFC[i].charaterCount
+		);
+	}
 
-	success = getStringCounts_TinyUTF8(
-		decomposedUTF8TestString,
-		NULL, NULL, &countChars,
-		&error
-	);
+	for (size_t i = 0; testStringsNFD[i].string; i++) {
+		size_t count = 0;
+		Error_TinyUTF8 error = No_Error_TinyUTF8;
 
-	testAssertMsg(success && !error, "Error: %s", nameOfError_TinyUTF8(error));
-	testAssertMsg(countChars == 212, "Count: %zu", countChars);
+		bool success = getStringCounts_TinyUTF8(
+			testStringsNFD[i].string,
+			NULL, NULL,&count,
+			&error
+		);
+
+		testAssertMsg(success && !error,
+			"Error: %s", nameOfError_TinyUTF8(error)
+		);
+		testAssertMsg(count == testStringsNFD[i].charaterCount,
+			"Count: %zu, Expected: %zu",
+			count, testStringsNFD[i].charaterCount
+		);
+	}
+
 }
 
 
 int main( )
 {
-	test_countBytes();
+	test_count();
 	test_countCodePoints();
 	test_countCharacters();
 	return 0;
