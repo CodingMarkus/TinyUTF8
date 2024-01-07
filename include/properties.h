b1df4053ba530763b78c7dc647bc05e735d8839b
CodingMarkus <CodingMarkus.Commit.2022@quantentunnel.de>
2024-01-07 04:55:07 +0100

    Add code documentation

--- include/properties.h
+++ include/properties.h
@@ -10,6 +10,15 @@ CodePointCategory_TinyUTF8 categoryOf_TinyUTF8 (
 	CodePoint_TinyUTF8 cp );
 
 
+/**
+	Whether the code point describes a printable character.
+	Code points that are intended to be combined with other code points to
+	form a single character, as well as control code points and code points
+	controlling text flow, are not considered printable characters. "Printable"
+	does not imply that anything is actually printed to as a space is also a
+	printable character, a line break on the other hand is not, as it is  a
+	control code point and controls text flow.
+*/
 bool isCharacter_TinyUTF8 ( CodePoint_TinyUTF8 cp );
 
 // ============================================================================
